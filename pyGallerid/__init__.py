181c1b8f433d0644c83aec93e686bafb291ff8e2
Benjamin Hepp <benjamin.hepp@gmail.com>
2012-01-30 20:21:12 +0100

    included pyramid_rewrite tween

--- pyGallerid/__init__.py
+++ pyGallerid/__init__.py
@@ -7,14 +7,11 @@ from pyramid_zodbconn import get_connection
 from pyramid.settings import asbool
 from pyramid.authentication import SessionAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
-from pyramid.security import authenticated_userid
-from pyramid.traversal import find_root
 
 from pyramid_beaker import session_factory_from_settings, \
                            set_cache_regions_from_settings
 
-from.models import appmaker, retrieve_user
-from models.user import groupfinder
+from.models import appmaker
 
 # This is only needed when using SQLAlchemy
 #from sqlalchemy import engine_from_config
@@ -39,24 +36,17 @@ def main(global_config, **settings):
 
     config = Configurator(root_factory=root_factory, settings=settings)
 
-    # add user property to request objects
-    def get_user(request):
-        root = find_root(request.context)
-        username = authenticated_userid(request)
-        user = retrieve_user(root, username)
-        return user
-    config.set_request_property(get_user, 'user', reify=True)
+    config.include(set_auth_policies)
 
-    # set up authentication and authorization
-    authn_policy = SessionAuthenticationPolicy(callback=groupfinder)
-    authz_policy = ACLAuthorizationPolicy()
-    config.set_authentication_policy(authn_policy)
-    config.set_authorization_policy(authz_policy)
+    config.include(add_user_property)
 
     # include pyramid plugins
+    import pyramid_rewrite
     config.include('pyramid_beaker')
     config.include('pyramid_tm')
     config.include('pyramid_zodbconn')
+    #config.include('pyramid_rewrite')
+    config.include(pyramid_rewrite)
 
     # register beaker session factory
     config.set_session_factory(session_factory)
@@ -65,6 +55,35 @@ def main(global_config, **settings):
     #engine = engine_from_config(settings, 'sqlalchemy.')
     #DBSession.configure(bind=engine)
 
+    config.include(add_views)
+
+    config.scan()
+
+    return config.make_wsgi_app()
+
+
+def add_user_property(config):
+    from pyramid.traversal import find_root
+    from pyramid.security import authenticated_userid
+    from models import retrieve_user
+    # add user property to request objects
+    def get_user(request):
+        root = find_root(request.context)
+        username = authenticated_userid(request)
+        user = retrieve_user(root, username)
+        return user
+    config.set_request_property(get_user, 'user', reify=True)
+
+def set_auth_policies(config):
+    from models.user import groupfinder
+    # set up authentication and authorization
+    authn_policy = SessionAuthenticationPolicy(callback=groupfinder)
+    authz_policy = ACLAuthorizationPolicy()
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
+
+def add_views(config):
+    settings = config.registry.settings
     config.add_static_view(
         'static',
         settings['static_dir'],
@@ -75,8 +94,4 @@ def main(global_config, **settings):
         settings['image_dir'],
         cache_max_age=3600
     )
-
-    config.scan()
-
-    return config.make_wsgi_app()
 
